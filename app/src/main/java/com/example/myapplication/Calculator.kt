c24ed8cb9580a4a7052e5ad8557c37cb35842d83
Andrei Leushin <yeahshaq@gmail.com>
2022-08-17 22:40:43 +0300

    homework2

--- app/src/main/java/com/example/myapplication/Calculator.kt
+++ app/src/main/java/com/example/myapplication/Calculator.kt
@@ -3,32 +3,33 @@ package com.example.myapplication
 import java.util.*
 
 object Calculator {
-    fun ExpressionToRpn(expr: String): String {
+    private fun expressionToRpn(expr: String): String {
         var current = ""
         val stack = Stack<Char>()
-        var priority: Int
         for (i in 0 until expr.length) {
-            priority = getP(expr[i])
+            val priority = getPriority(expr[i])
             if (priority == 0) current += expr[i]
             if (priority == 1) stack.push(expr[i])
             if (priority > 1) {
                 current += ' '
                 while (!stack.empty()) {
-                    current += if (getP(stack.peek()) >= priority) stack.pop() else break
+                    current += if (getPriority(stack.peek()) >= priority) stack.pop() else break
                 }
                 stack.push(expr[i])
             }
             if (priority == -1) {
                 current += ' '
-                while (getP(stack.peek()) != 1) current += stack.pop()
+                while (getPriority(stack.peek()) != 1) current += stack.pop()
                 stack.pop()
             }
         }
-        while (!stack.empty()) current += stack.pop()
+        while (!stack.empty()) {
+            current += stack.pop()
+        }
         return current
     }
 
-    fun RpnToAnswer(rpn: String): Double {
+    private fun rpnToAnswer(rpn: String): Double {
         var oper = String()
         val stack = Stack<Double>()
         var i = 0
@@ -37,15 +38,15 @@ object Calculator {
                 i++
                 continue
             }
-            if (getP(rpn[i]) == 0) {
-                while (rpn[i] != ' ' && getP(rpn[i]) == 0) {
+            if (getPriority(rpn[i]) == 0) {
+                while (rpn[i] != ' ' && getPriority(rpn[i]) == 0) {
                     oper += rpn[i++]
                     if (i == rpn.length) break
                 }
                 stack.push(oper.toDouble())
                 oper = String()
             }
-            if (getP(rpn[i]) > 1) {
+            if (getPriority(rpn[i]) > 1) {
                 val x = stack.pop()
                 val y = stack.pop()
                 if (rpn[i] == '+') stack.push(y + x)
@@ -58,13 +59,21 @@ object Calculator {
         return stack.pop()
     }
 
-    private fun getP(token: Char): Int {
+    private fun getPriority(token: Char): Int {
         return when (token) {
             '*', '/' -> 3
             '+', '-' -> 2
             '(' -> 1
             ')' -> -1
             else -> 0
+        }
+    }
+
+    fun result(string: String): String {
+        try {
+            return rpnToAnswer(expressionToRpn(string)).toString()
+        } catch (e: EmptyStackException) {
+            return ""
         }
     }
 }
